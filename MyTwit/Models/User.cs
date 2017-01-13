e8dbc65dda17f8dc8120079025f163b9c6d9591d
Cossplay <extreme-10@mail.ru>
2017-01-13 20:52:37 +0300

    CreateMd5

--- MyTwit/Models/User.cs
+++ MyTwit/Models/User.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace MyTwit.Models
 {
     public class User
     {
-        private int test;
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public static string CreateMd5(string input)
+        {
+            using (var md5Hash = MD5.Create())
+            {
+                var data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                var sb = new StringBuilder();
+                foreach (var b in data)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        public static bool VerifyMd5Hash(string input, string hash)
+        {
+            var hashOfinput = CreateMd5(input);
+            return hash.Equals(hash);
+        }
     }
 }
